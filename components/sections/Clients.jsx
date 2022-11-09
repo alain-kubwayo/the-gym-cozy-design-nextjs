a00372bf9fb658a8fc6c5086498711ecf683830b
alain-kubwayo <ahkubwayo@gmail.com>
2022-11-09 21:10:13 +0200

    fix: escape linting errors

--- components/sections/Clients.jsx
+++ components/sections/Clients.jsx
@@ -19,7 +19,7 @@ const Clients = () => {
                                 <Image src="/assets/clients/client-1.webp" width={80} height={80} alt="Avatar" className="w-full h-full" />
                             </div>
                             <p className="text-xl text-primary-300 font-fraunces72">“One of the top design professionals…”</p>
-                            <p className="text-lg text-primary-100 font-fraunces72">I connect with hundreds of talented folks on a regular basis, and I can say with complete confidence that Lee is one of the best UX designers in Aquent/Vitamin T's global network.</p>
+                            <p className="text-lg text-primary-100 font-fraunces72">I connect with hundreds of talented folks on a regular basis, and I can say with complete confidence that Lee is one of the best UX designers in Aquent/Vitamin T&apos;'s global network.</p>
                             <h5 className="text-xs uppercase font-geomanist text-primary-100">SUSIE POLLASKY</h5>
                             <p className="text-xs font-geomanist text-primary-100">Now: Leadership Recruiting @ Product Design at Facebook</p>
                         </div>
@@ -27,8 +27,8 @@ const Clients = () => {
                             <div className="flex items-center self-center justify-center overflow-hidden rounded-32% border-2.5 border-white w-20 h-20">
                                 <Image src="/assets/clients/client-2.webp" width={80} height={80} alt="Avatar" className="w-full h-full" />
                             </div>
-                            <p className="text-xl text-primary-300 font-fraunces72">“They’re a very well-rounded organization…”</p>
-                            <p className="text-lg text-primary-100 font-fraunces72">Cost per conversion and all of those metrics have dropped significantly. We used to pay an average of $100 per lead that converts… now we have that  down to about $20. Sessions and contacts have also gone up astronomically, at least threefold. Organic search and social media referrals have all continued to skyrocket. We’re really pleased.</p>
+                            <p className="text-xl text-primary-300 font-fraunces72">“They&apos;’re a very well-rounded organization…”</p>
+                            <p className="text-lg text-primary-100 font-fraunces72">Cost per conversion and all of those metrics have dropped significantly. We used to pay an average of $100 per lead that converts… now we have that  down to about $20. Sessions and contacts have also gone up astronomically, at least threefold. Organic search and social media referrals have all continued to skyrocket. We&apos;’re really pleased.</p>
                             <h5 className="text-xs uppercase font-geomanist text-primary-100">SUSIE POLLASKY</h5>
                             <p className="text-xs font-geomanist text-primary-100">Now: Leadership Recruiting @ Product Design at Facebook</p>
                         </div>
@@ -37,8 +37,8 @@ const Clients = () => {
                             <div className="flex items-center self-end justify-center overflow-hidden rounded-32% border-2.5 border-white w-20 h-20">
                                 <Image src="/assets/clients/client-3.webp" width={80} height={80} alt="Avatar" className="w-full h-full" />
                             </div>
-                            <p className="text-xl text-primary-300 font-fraunces72">“They're just good people.”</p>
-                            <p className="text-lg text-primary-100 font-fraunces72">They were a pleasure to work with and I'm really happy with the results. They pretty much nailed it.</p>
+                            <p className="text-xl text-primary-300 font-fraunces72">“They&apos;'re just good people.”</p>
+                            <p className="text-lg text-primary-100 font-fraunces72">They were a pleasure to work with and I&apos;'m really happy with the results. They pretty much nailed it.</p>
                             <h5 className="text-xs uppercase font-geomanist text-primary-100">SUSIE POLLASKY</h5>
                             <p className="text-xs font-geomanist text-primary-100">Now: Leadership Recruiting @ Product Design at Facebook</p>
                         </div>
